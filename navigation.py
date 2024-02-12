cd4f31678ce8c96fdecf697964e60072997c7f23
neoxak <neoxak@gmail.com>
2024-02-13 00:32:36 +0300

    fix mouse move

--- navigation.py
+++ navigation.py
@@ -1,6 +1,9 @@
 import random
 import time
 
+import numpy as np
+import scipy.interpolate as si
+
 from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver import ActionChains, Keys
@@ -374,11 +377,49 @@ def check_dialog_class(driver: webdriver.Chrome, mode):
                 action.pointer_action.move_to(but_x).pointer_down().move_by(2, 2)
                 action.perform()
                 time.sleep(time_mls)
-                action.pointer_action.move_to(but_x).pointer_down().pointer_up( )
+                action.pointer_action.move_to(but_x).pointer_down().pointer_up()
                 action.perform()
     except:
         # secure.log.write_log('Exception', e)
         pass
+
+
+def interpolate_move():
+    # Curve base:
+    points = [[0, 0], [0, 2], [2, 3], [4, 0], [6, 3], [8, 2], [8, 0]]
+    points = np.array(points)
+
+    x = points[:, 0]
+    y = points[:, 1]
+
+    t = range(len(points))
+    ipl_t = np.linspace(0.0, len(points) - 1, 100)
+
+    x_tup = si.splrep(t, x, k=3)
+    y_tup = si.splrep(t, y, k=3)
+
+    x_list = list(x_tup)
+    xl = x.tolist()
+    x_list[1] = xl + [0.0, 0.0, 0.0, 0.0]
+
+    y_list = list(y_tup)
+    yl = y.tolist()
+    y_list[1] = yl + [0.0, 0.0, 0.0, 0.0]
+
+    x_i = si.splev(ipl_t, x_list)  # x interpolate values
+    y_i = si.splev(ipl_t, y_list)  # y interpolate values
+    return [x_i, y_i]
+
+
+def move_mouse(action, start_element):
+    # First, go to your start point or Element:
+    action.move_to_element(start_element)
+    action.perform()
+
+    for mouse_x, mouse_y in zip(interpolate_move()[0], interpolate_move()[1]):
+        action.move_by_offset(mouse_x, mouse_y)
+        action.perform()
+        print(mouse_x, mouse_y)
 
 
 # Функция для выполнения проверки в отдельном потоке
