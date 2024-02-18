693c53a34c1bbf4c52a8807bab121fb9665c047a
neoxak <neoxak@gmail.com>
2024-02-18 03:09:41 +0300

    work to serv

--- selen.py
+++ selen.py
@@ -23,10 +23,10 @@ def get_path_webdriver():
 
 
 def set_driver_options(options):
-    # options.binary_location = "/usr/bin/chromium"
-    options.binary_location = "/usr/bin/chromium-browser"
-    # options.add_argument('--headless=new')
-    # options.add_argument("--no-sandbox")
+    options.binary_location = "/usr/bin/chromium"
+    # options.binary_location = "/usr/bin/chromium-browser"
+    options.add_argument('--headless=new')
+    options.add_argument("--no-sandbox")
     options.add_experimental_option("prefs", {
         "profile.default_content_setting_values.notifications": 2,
     })
